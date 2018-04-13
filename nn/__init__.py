e533d1e622e751a9021c2538debd3457c9d3f0e1
Rees <rees@pimloc.com>
2018-04-13 09:54:29 +0100

    Added forward softmax function and test code

--- nn/__init__.py
+++ nn/__init__.py
@@ -1,5 +1,6 @@
 import multilayer_perceptron as mlp
 import optimiser as opt
+
 import loss_functions as lsf
 import standard_layers as std
 import activation_functions as act
--- nn/loss_functions.py
+++ nn/loss_functions.py
@@ -18,3 +18,49 @@ class MSELoss():
     def backward(self):
         out = self.x - self.gt
         return out
+
+
+class Softmax():
+    def __init__(self):
+        self.x = 0
+        self.stability_factor = 0.2
+
+    def __call__(self, x):
+        return self.forward(x)
+
+    def forward(self, x):
+        """
+        input x will be an Nx1 row array
+        softmax function has the form e^fi/SUM_over_f e^fi
+        with loss, has the form:
+            Loss = -log(softmax)
+
+            for stability:
+                e^fi * e^N/SUM(e^fi) * e^N
+                then same as
+                e^(fi + s)/SUM(e^(fi+s))
+        """
+        self.x = x.copy()
+        numerator = np.exp(x + self.stability_factor)
+        denominator = np.sum(np.exp(x + self.stability_factor))
+        y = numerator / denominator
+        return y
+
+
+def test_softmax():
+    N = 5
+    x = np.ones((N, 1))
+    softmax = Softmax()
+    y = softmax(x)
+    print 'x ', x
+    print 'y ', y
+    print np.sum(y)
+    x = np.array([[0.2, 1.0, 0.2, 0.2, 0.2, 0.2]])
+    print x
+    y = softmax(x)
+    print y
+    print np.sum(y)
+
+
+if __name__ == '__main__':
+    test_softmax()
